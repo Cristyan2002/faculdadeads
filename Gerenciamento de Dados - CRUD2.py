05c07ec6bbfe49d5157bd1a81412e112c58f2c6b
Cristyan Silva <cristian.silvarosario@gmail.com>
2024-09-25 17:56:47 -0300

    Gerenciamento de Dados Utilizando JSON
    Utilizando JSON para guardar as listas e os estudantes.

--- Gerenciamento de Dados - CRUD2.py
+++ Gerenciamento de Dados - CRUD2.py
@@ -1,3 +1,4 @@
+import json
 # Criando a função do Menu de operações:
 def menu_de_operacoes():
     print('-' * 10, 'Olá, Bem vindo ao Menu Principal', '-' * 10)
@@ -12,14 +13,14 @@ def menu_de_operacoes_secundario():
                 print('-' * 10, f'{escolha_texto} : Menu de Operações', '-' * 10)
                 print('(1) - Incluir')
                 print('(2) - Listar')
-                print('(3) - Atualizar')
-                print('(4) - Exluir')
-                print('(5) - Editar Usuário')
-                print('(6) - Voltar ao Menu Principal')
+                print('(3) - Exluir')
+                print('(4) - Editar Usuário')
+                print('(5) - Voltar ao Menu Principal')
 # Criando a funçao de Incluir (Cadastrar) os Estudantes:
-def incluir_estudantes(estudantes):
+def incluir_estudantes(salvar):
     escolha_texto2 = '(1) - Incluir'
     print(f'Você escolheu a opção: {escolha_texto2}')
+    listas_estudantes = ler_arquivo(salvar)
     codigo = int(input('Digite o código do Estudante: '))
     nome_estudantes = input('Digite o nome do Aluno: ')
     cpf = int(input('Por favor digite o CPF: '))
@@ -29,27 +30,29 @@ def incluir_estudantes(estudantes):
     'nome_estudante' : nome_estudantes,
     'cpf_estudante' : cpf
     }
-    estudantes.append(dados_estudantes)
+    listas_estudantes.append(dados_estudantes)
     print('Pronto, você está Incluído na Lista!.')
     sleep(2)
+    salvar_arquivo(listas_estudantes, salvar)
 # Criando a função de mostrar as listas dos Estudantes, que foram cadastrados:
-def lista_dos_estudantes(estudantes):
+def lista_dos_estudantes(salvar):
+    listas_estudantes = ler_arquivo(salvar)
     escolha_texto2 = '(2) - Listar'
     print(f'Você escolheu a opção: {escolha_texto2}')
-    if len (estudantes) == 0:
+    if len (listas_estudantes) == 0:
        print('Não há Estudantes Cadastrados!.')
     else:
-        for lista in estudantes:
-          print('Os estudantes cadastrados são:\n{}'.format(estudantes))
+        for lista in listas_estudantes:
+          print('Os estudantes cadastrados são:\n{}'.format(listas_estudantes))
           sleep(2)
           break
 # Criando a função de excluir os Estudantes através do código.
-def excluir_estudantes(estudantes):
-    escolha_texto2 = '(4) - Excluir'
+def excluir_estudantes(salvar):
+    escolha_texto2 = '(3) - Excluir'
     print(f'Você escolheu a opção: {escolha_texto2}')
     codigo_estudante = int(input('Qual o código do aluno que deseja remover? '))
     estudante_excluir = None
-    for dados_estudantes in estudantes:
+    for dados_estudantes in listas_estudantes:
       if dados_estudantes['codigo_estudante'] == codigo_estudante:
          estudante_excluir = dados_estudantes
          print('Removendo só um instante....')
@@ -59,32 +62,53 @@ def excluir_estudantes(estudantes):
       if estudante_excluir == None:
          print('Não entendi esse código, de uma olhadinha nos alunos cadastrados.')
          sleep(2)
-         print(estudantes)
+         print(listas_estudantes)
          break
-    estudantes.remove(estudante_excluir)
+    listas_estudantes = ler_arquivo(salvar)
+    listas_estudantes.remove(estudante_excluir)
+    salvar_arquivo(listas_estudantes, salvar)
+    
 # Criando a função de editar um Estudante através do seu código.
-def editar_estudantes(estudantes):
-    escolha_texto2 = ('(5) - Editar Usuário')
+def editar_estudantes(salvar):
+    escolha_texto2 = ('(4) - Editar Usuário')
     print(f'Você escolheu a opção: {escolha_texto2}')
-    codigo_estudante = int(input('Qual o código do aluno que deseja editar ?'))
-    estudante_editar = None
-    for editor_estudante in estudantes:
-        if editor_estudante['codigo_estudante'] == codigo_estudante:
-            estudante_editar = editor_estudante
-        if estudante_editar == None:
-            print('Não achei nenhum usuário com esse código.')
+    listas_estudantes = ler_arquivo(salvar)
+    if len(listas_estudantes) == 0:
+            print('Nenhum estudantes está cadastrado.')
             sleep(2)
-            break
-        else:
-            estudante_editar['codigo_estudante'] = int(input('Digite um novo Código: '))
-            estudante_editar['nome_estudante'] = int(input('Digite um novo Nome: '))
-            estudante_editar['cpf_estudante'] = int(input('Digite um novo CPF: '))
-            print('Edição concluída com sucesso!')
-            sleep(2)
+    else:
+        codigo_estudante = int(input('Qual o código do aluno que deseja editar ?'))
+        estudante_editar = None
+        for editor_estudante in listas_estudantes:
+            if editor_estudante['codigo_estudante'] == codigo_estudante:
+                estudante_editar = editor_estudante
+            if estudante_editar == None:
+                print('Não achei nenhum usuário com esse código.')
+                sleep(2)
+                break
+            else:
+                estudante_editar['codigo_estudante'] = int(input('Digite um novo Código: '))
+                estudante_editar['nome_estudante'] = int(input('Digite um novo Nome: '))
+                estudante_editar['cpf_estudante'] = int(input('Digite um novo CPF: '))
+                salvar_arquivo(listas_estudantes, salvar)
+                print('Edição concluída com sucesso!')
+                sleep(2)
+#Criando um arquivo para salvar JSON.
+def salvar_arquivo(listas_estudantes, salvar):
+    with open(salvar, 'w') as open_file:
+        json.dump(listas_estudantes, open_file)
+
+#Para ler a lista em JSON.
+def ler_arquivo(salvar):
+    try:
+        with open(salvar, 'r') as open_file:
+            listas_estudantes = json.load(open_file)
+        return listas_estudantes
+    except: []
 # Vou importar o sleep para o programa ficar um pouco mais dinâmico!
 from time import sleep
 #Lista para cada estudante adicionado!
-estudantes = []
+arquivo_estudante = 'estudantes.json'
 while True:
     menu_de_operacoes()
     # O Usuário irá selecionar a opção desejada, e consequentemente, irá para tela do menu de operações. #
@@ -111,18 +135,14 @@ while True:
 
                 if escolha2 >= 1 and escolha <= 6:
                     if escolha2 == 1:
-                        incluir_estudantes(estudantes)
+                        incluir_estudantes(arquivo_estudante)
                     elif escolha2 == 2:
-                        lista_dos_estudantes(estudantes)
+                        lista_dos_estudantes(arquivo_estudante)
                     elif escolha2 == 3:
-                        escolha_texto2 = '(3) - Atualizar'
-                        print('Desculpe, esta função está em desenvolvimento...!')
-                        sleep(2)
+                        excluir_estudantes(arquivo_estudante)
                     elif escolha2 == 4:
-                        excluir_estudantes(estudantes)
+                        editar_estudantes(arquivo_estudante)
                     elif escolha2 == 5:
-                        editar_estudantes(estudantes)
-                    elif escolha2 == 6:
                         print('Você escolheu a opção: Voltar ao Menu Principal')
                         print('Voltando...')
                         sleep(2)
